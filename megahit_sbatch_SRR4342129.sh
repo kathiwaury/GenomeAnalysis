1ba06371e699d66db256a7e635a57932116eb64d
Katharina Waury <kathwaur@IBG-D0022.ibg.uu.se>
2019-04-07 15:28:29 +0200

    sbatch script for genome assembly 190407-2

--- megahit_sbatch_SRR4342129.sh
+++ megahit_sbatch_SRR4342129.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 18:00:00
-#SBATCH -J 190406_megahit_KW_SRR4342129
+#SBATCH -J 190407_megahit_KW_SRR4342129
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user kathiwaury@gmail.com
 
@@ -13,4 +13,4 @@ module load bioinfo-tools
 module load megahit
 
 # your commands
-megahit -1 ~/data/raw/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz -2 ~/data/raw/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz -o ~/results/megahit_SRR4342129 --kmin-1pass --k-max 105 --k-step 10 -t 2
+megahit -1 ~/data/raw/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz -2 ~/data/raw/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz -o ~/results/megahit_SRR4342129 --kmin-1pass --k-min 65 --k-max 105 --k-step 10 -t 2
