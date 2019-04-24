c0f17d08dd63d21354cc001edc1deb7019bc5ff1
Katharina Waury <kathwaur@IBG-D0037.ibg.uu.se>
2019-04-24 10:33:28 +0200

    CheckM scripts

---
+++ checkm_sbatch_SRR4342133.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l 
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 04:00:00
+#SBATCH -J 190424_checkm_KW_SRR4342133
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user kathiwaury@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load CheckM
+
+# Your commands
+
+checkm lineage_wf -x fa -t 4 --reduced_tree ~/results/metabat_SRR4342133/ ~/results/checkm_SRR4342133/
