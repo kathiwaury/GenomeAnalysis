bb7b1c3ea72cc01ab2003c0262aa555a032ea8f7
Katharina Waury <kathwaur@IBG-D0037.ibg.uu.se>
2019-04-24 09:53:02 +0200

    metaquast sbtach scripts

---
+++ metaquast_sbatch_SRR4342129.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 2:00:00
+#SBATCH -J 190424_metaquast_KW_SRR4342129
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user kathiwaury@gmail.com
+
+# load modules
+module load bioinfo-tools
+module load quast
+
+# your commands
+metaquast.py ~/results/megahit_SRR4342129/final.contigs.fa -o ~/results/metaquast_SRR4342129 -t 2 -1 ~/data/raw/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz -2 ~/data/raw/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz
