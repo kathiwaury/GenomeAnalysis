06dd8676e6df9e464730ba5ea231603c3451747a
Katharina Waury <kathwaur@IBG-D0037.ibg.uu.se>
2019-05-13 17:59:14 +0200

    phylophlan scripts

---
+++ phylophlan_sbatch_SRR4342129.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l 
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 08:00:00
+#SBATCH -J 190513_phylophlan_KW_SRR4342129
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user kathiwaury@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load bwa
+module load python
+module load biopython
+module load FastTree
+module load muscle
+module load usearch/5.2.32
+
+# Your commands
+~/nsegata-phylophlan-1d174e34b2ae/phylophlan.py -u metagenome_29
