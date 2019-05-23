8ab765c1673ab4518b55b4ec956c0adf31ed14ef
kathiwaury <47524923+kathiwaury@users.noreply.github.com>
2019-05-23 22:24:08 +0200

    phylophlan without -t

--- phylophlan_sbatch_SRR4342133.sh
+++ phylophlan_sbatch_SRR4342133.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core
 #SBATCH -n 4
 #SBATCH -t 12:00:00
-#SBATCH -J 190517_phylophlan_KW_SRR4342133
+#SBATCH -J 190523_phylophlan_KW_SRR4342133
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user kathiwaury@gmail.com
 
@@ -18,4 +18,4 @@ module load usearch/5.2.32
 
 # Your commands
 cd ~/nsegata-phylophlan-1d174e34b2ae
-./phylophlan.py -i -t --nproc 4  metagenome_33
+./phylophlan.py -i --nproc 4  metagenome_33
