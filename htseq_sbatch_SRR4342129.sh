05d52de7f167ade844e52e84427bc87cdb086feb
kathiwaury <47524923+kathiwaury@users.noreply.github.com>
2019-05-27 12:28:40 +0200

    Update htseq_sbatch_SRR4342129.sh

--- htseq_sbatch_SRR4342129.sh
+++ htseq_sbatch_SRR4342129.sh
@@ -13,24 +13,24 @@ module load bioinfo-tools
 module load htseq
 
 # Your commands  
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-1_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-1/annotation_SRR4342129_bin-1_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-1_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-1/annotation_SRR4342129_bin-1_out.gff > htseq_SRR4342129_bin-1.out
 
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-2_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-2/annotation_SRR4342129_bin-2_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-2_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-2/annotation_SRR4342129_bin-2_out.gff > htseq_SRR4342129_bin-2.out
 
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-3_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-3/annotation_SRR4342129_bin-3_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-3_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-3/annotation_SRR4342129_bin-3_out.gff > htseq_SRR4342129_bin-3.out
 
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-4_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-4/annotation_SRR4342129_bin-4_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-4_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-4/annotation_SRR4342129_bin-4_out.gff > htseq_SRR4342129_bin-4.out
 
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-5_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-5/annotation_SRR4342129_bin-5_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-5_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-5/annotation_SRR4342129_bin-5_out.gff > htseq_SRR4342129_bin-5.out
 
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-6_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-6/annotation_SRR4342129_bin-6_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-6_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-6/annotation_SRR4342129_bin-6_out.gff > htseq_SRR4342129_bin-6.out
 
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-7_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-7/annotation_SRR4342129_bin-7_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-7_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-7/annotation_SRR4342129_bin-7_out.gff > htseq_SRR4342129_bin-7.out
 
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-8_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-8/annotation_SRR4342129_bin-8_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-8_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-8/annotation_SRR4342129_bin-8_out.gff > htseq_SRR4342129_bin-8.out
 
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-9_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-9/annotation_SRR4342129_bin-9_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-9_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-9/annotation_SRR4342129_bin-9_out.gff > htseq_SRR4342129_bin-9.out
 
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-10_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-10/annotation_SRR4342129_bin-10_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-10_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-10/annotation_SRR4342129_bin-10_out.gff > htseq_SRR4342129_bin-10.out
 
-htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-11_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-11/annotation_SRR4342129_bin-11_out.gff
+htseq-count -f bam  -r pos -t  CDS  -i  ID -s no ~/results/bwa_SRR4342129/bin_SRR4342129-11_mapped.sorted.bam  ~/results/prokka_SRR4342129/prokka_SRR4342129_bin-11/annotation_SRR4342129_bin-11_out.gff > htseq_SRR4342129_bin-11.out
